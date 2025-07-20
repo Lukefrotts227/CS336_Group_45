74077931abe95767abb07641581206f2b7885345
Ljf227 <ljfrotton@gmail.com>
2025-07-20 14:58:15 -0400

    servlet

--- src/main/java/com/cs336/pkg/CustomerServlet.java
+++ src/main/java/com/cs336/pkg/CustomerServlet.java
@@ -3,8 +3,7 @@ package com.cs336.pkg;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class CustomerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -50,9 +49,53 @@ public class CustomerServlet extends HttpServlet {
 	            request.getRequestDispatcher("customer/customerdashboard.jsp").forward(request, response);
 	            return;
 	        }
+			else if ("reserve".equals(action)){
+				String sh_Id = request.getParameter("scheduleId");
+				String category = request.getParameter("category");
+				if(sh_Id == null || category == null ){
+					request.setAttribute("error", "Missing schedule ID or category.");
+					request.getRequestDispatcher("customer/makeReservation.jsp").forward(request, response);
+					return;
+				}
 
+				int scheduleId = Integer.parseInt(sh_Id);
+				double rate = 0.0;
+
+				switch (category) {
+					case "child": rate = .75; break;
+					case "disabled": rate = .50; break;
+					case "elderly": rate = .65; break;
+					default: rate = 1.0; category = "none"; 
+				}
+				try (Connection conn = new ApplicationDB().getConnection()) {
+					String fareSql =
+			"SELECT fare, departure_date_time " +
+			"FROM train_schedules " +
+			"WHERE schedule_id = ?";
+		
+				
+				double baseFare; 
+				Timestamp departTime; 
+
+				try(PreparedStatement pf = conn.prepareStatement(fareSql)){
+
+					pf.setInt(1, scheduleId); 
+					try(ResultSet rs = pf.executeQuery()){
+						if(!rs.next()) throw new SQLException("Could not find schedule"); 
+						rate = rs.getDouble("fare"); 
+						departTime = rs.getTimestamp("departure_date_time"); 
+					}
+				}
+
+				
+
+				}
+			}
+
+			else{
 	        // For unknown actions, redirect back to dashboard
 	        response.sendRedirect("customer/customerdashboard.jsp");
-	    }
-
-}
+			}
+			
+	}
+}		
